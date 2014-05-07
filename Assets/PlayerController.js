eac73e36021b6115f94ab89ceba37e8b7b01c739
dentaq <katsu.home@gmail.com>
2014-05-07 22:17:06 +0900

    first commit

---
+++ Assets/PlayerController.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+animation.Play("Walk");
+yield WaitForSeconds(4.0);
+animation.Play("Crouch");
+yield WaitForSeconds(4.0);
+animation.Play("Idle");
+}
+
+function Update () {
+
+}
